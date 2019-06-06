b13c710f7a1fa77f8d0d506b0fb5c77aa9e0170c
0xBAMA <jon.baker1222@gmail.com>
2019-06-05 23:41:10 -0400

    new demo

--- code/generate.cc
+++ code/generate.cc
@@ -47,60 +47,142 @@ int main()
 
   tick = Clock::now();
 
+// PALETTE TEST
+  // vec loc1 = vec(30, 30, 30);
+  // vec loc2 = vec(30, 30,127);
+  // vec loc3 = vec(30, 30,225);
+  //
+  // vec loc4 = vec(30,127, 30);
+  // vec loc5 = vec(30,127,127);
+  // vec loc6 = vec(30,127,225);
+  //
+  // vec loc7 = vec(30,225, 30);
+  // vec loc8 = vec(30,225,127);
+  // vec loc9 = vec(30,225,225);
+  //
+  //
+  //
+  // for(int num = 0; num <= 78; num++)
+  // {
+  //   // main_block->draw_sphere(loc1 + vec(5*num,std::rand()%10-5,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
+  //   // main_block->draw_sphere(loc2 + vec(5*num,std::rand()%10-5,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
+  //   // main_block->draw_sphere(loc3 + vec(5*num,std::rand()%10-5,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
+  //
+  //   main_block->draw_sphere(loc1 + vec(5.75*num,0,0), 5, main_block->get_vox(num,255,false));
+  //   main_block->draw_sphere(loc2 + vec(5.75*num,0,0), 5, main_block->get_vox(num,255,false));
+  //   main_block->draw_sphere(loc3 + vec(5.75*num,0,0), 5, main_block->get_vox(num,255,false));
+  //
+  //   main_block->draw_sphere(loc4 + vec(5.75*num,std::rand()%15-7.5,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
+  //   main_block->draw_sphere(loc5 + vec(5.75*num,std::rand()%15-7.5,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
+  //   main_block->draw_sphere(loc6 + vec(5.75*num,std::rand()%15-7.5,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
+  //
+  //   main_block->draw_sphere(loc7 + vec(5.75*num,std::rand()%20-10,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
+  //   main_block->draw_sphere(loc8 + vec(5.75*num,std::rand()%20-10,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
+  //   main_block->draw_sphere(loc9 + vec(5.75*num,std::rand()%20-10,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
+  //
+  // }
+  //
+  // main_block->mask_all_nonzero();
+  //
+  // main_block->draw_sphere(vec(256,127,127), 74.9, main_block->get_vox(62,10,false));
+  //
+  // main_block->draw_cylinder(loc1-vec(30,0,0), loc1+vec(500,0,0),1, main_block->get_vox(62,255,false));
+  // main_block->draw_cylinder(loc2-vec(30,0,0), loc2+vec(500,0,0),1, main_block->get_vox(62,255,false));
+  // main_block->draw_cylinder(loc3-vec(30,0,0), loc3+vec(500,0,0),1, main_block->get_vox(62,255,false));
+  //
+  // main_block->draw_cylinder(loc4-vec(30,0,0), loc4+vec(500,0,0),1, main_block->get_vox(62,255,false));
+  // main_block->draw_cylinder(loc5-vec(30,0,0), loc5+vec(500,0,0),1, main_block->get_vox(62,255,false));
+  // main_block->draw_cylinder(loc6-vec(30,0,0), loc6+vec(500,0,0),1, main_block->get_vox(62,255,false));
+  //
+  // main_block->draw_cylinder(loc7-vec(30,0,0), loc7+vec(500,0,0),1, main_block->get_vox(62,255,false));
+  // main_block->draw_cylinder(loc8-vec(30,0,0), loc8+vec(500,0,0),1, main_block->get_vox(62,255,false));
+  // main_block->draw_cylinder(loc9-vec(30,0,0), loc9+vec(500,0,0),1, main_block->get_vox(62,255,false));
 
-  vec loc1 = vec(30, 30, 30);
-  vec loc2 = vec(30, 30,127);
-  vec loc3 = vec(30, 30,225);
 
-  vec loc4 = vec(30,127, 30);
-  vec loc5 = vec(30,127,127);
-  vec loc6 = vec(30,127,225);
+  vec a,b,c,d,e,f,g,h,v;
 
-  vec loc7 = vec(30,225, 30);
-  vec loc8 = vec(30,225,127);
-  vec loc9 = vec(30,225,225);
+  a = vec( 245,225,225 );
+  b = vec( 245, 30,225 );
+  c = vec( 270,225,225 );
+  d = vec( 270, 30,225 );
+  e = vec( 245,225, 30 );
+  f = vec( 245, 30, 30 );
+  g = vec( 270,225, 30 );
+  h = vec( 270, 30, 30 );
+
+  v = vec( 100,  0,  0 );
+
+  main_block->draw_quadrilateral_hexahedron( a, b, c, d, e, f, g, h, main_block->get_vox( 62, 255, false ) );
+  main_block->draw_quadrilateral_hexahedron( a+v, b+v, c+v, d+v, e+v, f+v, g+v, h+v, main_block->get_vox( 62, 255, false ) );
+  main_block->draw_quadrilateral_hexahedron( a-v, b-v, c-v, d-v, e-v, f-v, g-v, h-v, main_block->get_vox( 62, 255, false ) );
 
 
 
-  for(int num = 0; num <= 78; num++)
-  {
-    // main_block->draw_sphere(loc1 + vec(5*num,std::rand()%10-5,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
-    // main_block->draw_sphere(loc2 + vec(5*num,std::rand()%10-5,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
-    // main_block->draw_sphere(loc3 + vec(5*num,std::rand()%10-5,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
+  a = vec( 254,225,225 );
+  b = vec( 254, 30,225 );
+  c = vec( 259,225,225 );
+  d = vec( 259, 30,225 );
+  e = vec( 254,225, 30 );
+  f = vec( 254, 30, 30 );
+  g = vec( 259,225, 30 );
+  h = vec( 259, 30, 30 );
 
-    main_block->draw_sphere(loc1 + vec(5.75*num,0,0), 5, main_block->get_vox(num,255,false));
-    main_block->draw_sphere(loc2 + vec(5.75*num,0,0), 5, main_block->get_vox(num,255,false));
-    main_block->draw_sphere(loc3 + vec(5.75*num,0,0), 5, main_block->get_vox(num,255,false));
+  main_block->draw_quadrilateral_hexahedron( a, b, c, d, e, f, g, h, main_block->get_vox(  8, 255, false ) );
+  main_block->draw_quadrilateral_hexahedron( a+v, b+v, c+v, d+v, e+v, f+v, g+v, h+v, main_block->get_vox(  8, 255, false ) );
+  main_block->draw_quadrilateral_hexahedron( a-v, b-v, c-v, d-v, e-v, f-v, g-v, h-v, main_block->get_vox(  8, 255, false ) );
 
-    main_block->draw_sphere(loc4 + vec(5.75*num,std::rand()%15-7.5,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
-    main_block->draw_sphere(loc5 + vec(5.75*num,std::rand()%15-7.5,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
-    main_block->draw_sphere(loc6 + vec(5.75*num,std::rand()%15-7.5,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
 
-    main_block->draw_sphere(loc7 + vec(5.75*num,std::rand()%20-10,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
-    main_block->draw_sphere(loc8 + vec(5.75*num,std::rand()%20-10,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
-    main_block->draw_sphere(loc9 + vec(5.75*num,std::rand()%20-10,std::rand()%20-10), 5, main_block->get_vox(num,255,false));
-
-  }
 
   main_block->mask_all_nonzero();
 
-  main_block->draw_sphere(vec(256,127,127), 74.9, main_block->get_vox(62,10,false));
+  main_block->mask_invert_mask();
 
-  main_block->draw_cylinder(loc1-vec(30,0,0), loc1+vec(500,0,0),1, main_block->get_vox(62,255,false));
-  main_block->draw_cylinder(loc2-vec(30,0,0), loc2+vec(500,0,0),1, main_block->get_vox(62,255,false));
-  main_block->draw_cylinder(loc3-vec(30,0,0), loc3+vec(500,0,0),1, main_block->get_vox(62,255,false));
+  main_block->draw_cylinder( vec( 130, 60, 60), vec( 390, 60, 60), 15, main_block->get_vox( 12, 5, false ) );
+  main_block->draw_cylinder( vec( 130, 95, 60), vec( 390, 95, 60), 10, main_block->get_vox( 11, 5, false ) );
+  main_block->draw_cylinder( vec( 130, 127.5, 60), vec( 390, 127.5, 60), 10, main_block->get_vox( 11, 5, false ) );
+  main_block->draw_cylinder( vec( 130,160, 60), vec( 390,160, 60), 10, main_block->get_vox( 11, 5, false ) );
+  main_block->draw_cylinder( vec( 130,195, 60), vec( 390,195, 60), 15, main_block->get_vox( 12, 5, false ) );
 
-  main_block->draw_cylinder(loc4-vec(30,0,0), loc4+vec(500,0,0),1, main_block->get_vox(62,255,false));
-  main_block->draw_cylinder(loc5-vec(30,0,0), loc5+vec(500,0,0),1, main_block->get_vox(62,255,false));
-  main_block->draw_cylinder(loc6-vec(30,0,0), loc6+vec(500,0,0),1, main_block->get_vox(62,255,false));
-
-  main_block->draw_cylinder(loc7-vec(30,0,0), loc7+vec(500,0,0),1, main_block->get_vox(62,255,false));
-  main_block->draw_cylinder(loc8-vec(30,0,0), loc8+vec(500,0,0),1, main_block->get_vox(62,255,false));
-  main_block->draw_cylinder(loc9-vec(30,0,0), loc9+vec(500,0,0),1, main_block->get_vox(62,255,false));
+  main_block->draw_cylinder( vec( 130, 60, 195), vec( 390, 60, 195), 15, main_block->get_vox( 12, 5, false ) );
+  main_block->draw_cylinder( vec( 130, 95, 195), vec( 390, 95, 195), 10, main_block->get_vox( 11, 5, false ) );
+  main_block->draw_cylinder( vec( 130, 127.5, 195), vec( 390, 127.5, 195), 10, main_block->get_vox( 11, 5, false ) );
+  main_block->draw_cylinder( vec( 130,160, 195), vec( 390,160, 195), 10, main_block->get_vox( 11, 5, false ) );
+  main_block->draw_cylinder( vec( 130,195, 195), vec( 390,195, 195), 15, main_block->get_vox( 12, 5, false ) );
 
 
 
-  // main_block->draw_noise();
+  main_block->draw_cylinder( vec( 130, 195, 160), vec( 390, 195, 160), 10, main_block->get_vox( 11, 5, false ) );
+  main_block->draw_cylinder( vec( 130, 195, 127.5), vec( 390, 195, 127.5), 10, main_block->get_vox( 11, 5, false ) );
+  main_block->draw_cylinder( vec( 130, 195, 95), vec( 390,195, 95), 10, main_block->get_vox( 11, 5, false ) );
+
+  main_block->draw_cylinder( vec( 130, 60, 160), vec( 390, 60, 160), 10, main_block->get_vox( 11, 5, false ) );
+  main_block->draw_cylinder( vec( 130, 60, 127.5), vec( 390, 60, 127.5), 10, main_block->get_vox( 11, 5, false ) );
+  main_block->draw_cylinder( vec( 130, 60, 95), vec( 390,60, 95), 10, main_block->get_vox( 11, 5, false ) );
+
+
+
+  main_block->draw_sphere( vec( 255.5, 127.5, 127.5 ), 25, main_block->get_vox( 34, 5, false ) );
+  main_block->draw_sphere( vec( 255.5, 127.5, 127.5 ) + v, 25, main_block->get_vox( 34, 5, false ) );
+  main_block->draw_sphere( vec( 255.5, 127.5, 127.5 ) - v, 25, main_block->get_vox( 34, 5, false ) );
+
+  main_block->mask_unmask_all();
+
+  main_block->draw_cylinder( vec( 390,225,225 ), vec( 130,225,225 ), 5, main_block->get_vox( 18, 200, false ) );
+  main_block->draw_cylinder( vec( 390, 30,225 ), vec( 130, 30,225 ), 5, main_block->get_vox( 18, 200, false ) );
+  main_block->draw_cylinder( vec( 390,225, 30 ), vec( 130,225, 30 ), 5, main_block->get_vox( 18, 200, false ) );
+  main_block->draw_cylinder( vec( 390, 30, 30 ), vec( 130, 30, 30 ), 5, main_block->get_vox( 18, 200, false ) );
+
+  main_block->draw_tube( vec( 405, 127.5, 127.5), vec( 102, 127.5, 127.5), 10, 15, main_block->get_vox( 19, 200, false ) );
+
+  main_block->mask_all_nonzero();
+
+  main_block->draw_sphere( vec( 255.5, 127.5, 127.5), 200, main_block->get_vox( 15, 1, false ) );
+
+
+
+
+
+
 
 
 // FRAMING
