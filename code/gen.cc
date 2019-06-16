ee11fd3ed7198268b80801ecd340544eff7dde7d
0xBAMA <jon.baker1222@gmail.com>
2019-06-16 01:00:29 -0400

    the compass rose

--- code/gen.cc
+++ code/gen.cc
@@ -17,6 +17,7 @@ using milliseconds=std::chrono::milliseconds;
 
 
 void draw_axes_and_corners();
+void draw_compass_rose();
 
 
 
@@ -51,179 +52,77 @@ int main()
 
 	vec a,b,c,d,e,f,g,h;
 
-	// note:
-	// 	a : -x, +y, +z
-	// 	b : -x, -y, +z
-	// 	c : +x, +y, +z
-	// 	d : +x, -y, +z
-	// 	e : -x, +y, -z
-	// 	f : -x, -y, -z
-	// 	g : +x, +y, -z
-	// 	h : +x, -y, -z
+
 
 	Vox material1  =  main_block->get_vox(58, 255.0, false);
-	Vox material2  =  main_block->get_vox(27, 255.0, false);
-	Vox material3  =  main_block->get_vox(28, 255.0, false);
-	Vox rust       =  main_block->get_vox(10, 255.0, false);
-	Vox rust2      =  main_block->get_vox( 9,   5.0, false);
-	Vox atmosphere =  main_block->get_vox(58,   3.0, false);
-	Vox water      =  main_block->get_vox(40,  20.0, false);
-	Vox shroud     =  main_block->get_vox(49,  20.0, false);
+	Vox material2  =  main_block->get_vox(62,  20.0, false);
 
-
-
-
-	cout << endl << "starting " << endl;
-
-
-
-
-	for(int x = 31; x < 512; x+=64)
-	{
-		for(int y = 31; y < 256; y+=64)
-		{
-
-			a = vec( x-7, y+7, 256);
-			b = vec( x-7, y-7, 256);
-			c = vec( x+7, y+7, 256);
-			d = vec( x+7, y-7, 256);
-			e = vec( x-7, y+7, 0);
-			f = vec( x-7, y-7, 0);
-			g = vec( x+7, y+7, 0);
-			h = vec( x+7, y-7, 0);
-
-			main_block->draw_quadrilateral_hexahedron( a, b, c, d, e, f, g, h, shroud);
-
-		}
-	}
-
-	cout << "xy plane done" << endl;
-
-	for(int x = 31; x < 512; x+=64)
-	{
-		for(int z = 31; z < 256; z+=64)
-		{
-
-			a = vec( x-7, 256, z+7);
-			b = vec( x-7,   0, z+7);
-			c = vec( x+7, 256, z+7);
-			d = vec( x+7,   0, z+7);
-			e = vec( x-7, 256, z-7);
-			f = vec( x-7,   0, z-7);
-			g = vec( x+7, 256, z-7);
-			h = vec( x+7,   0, z-7);
-
-			main_block->draw_quadrilateral_hexahedron( a, b, c, d, e, f, g, h, shroud);
-
-		}
-	}
-
-	cout << "xz plane done" << endl;
-
-	for(int y = 31; y < 256; y+=64)
-	{
-		for(int z = 31; z < 256; z+=64)
-		{
-
-			a = vec(   0, y+7, z+7);
-			b = vec(   0, y-7, z+7);
-			c = vec( 512, y+7, z+7);
-			d = vec( 512, y-7, z+7);
-			e = vec(   0, y+7, z-7);
-			f = vec(   0, y-7, z-7);
-			g = vec( 512, y+7, z-7);
-			h = vec( 512, y-7, z-7);
-
-			main_block->draw_quadrilateral_hexahedron( a, b, c, d, e, f, g, h, shroud);
-
-		}
-	}
-
-	cout << "yz plane done" << endl;
-
-
-	cout << endl << "moving to outer layers" << endl << endl;
 
 	//OUTER LAYERS
 
-	for(int x = 31; x < 512; x+=64)
+	for(int x = 223; x < 288; x+=64)
 	{
-		for(int y = 31; y < 256; y+=64)
+		for(int y = 95; y < 160; y+=64)
 		{
 
-			a = vec( x-5, y+5, 256);
-			b = vec( x-5, y-5, 256);
-			c = vec( x+5, y+5, 256);
-			d = vec( x+5, y-5, 256);
-			e = vec( x-5, y+5, 0);
-			f = vec( x-5, y-5, 0);
-			g = vec( x+5, y+5, 0);
-			h = vec( x+5, y-5, 0);
+			a = vec( x-4, y+4, 256);
+			b = vec( x-4, y-4, 256);
+			c = vec( x+4, y+4, 256);
+			d = vec( x+4, y-4, 256);
+			e = vec( x-4, y+4, 0);
+			f = vec( x-4, y-4, 0);
+			g = vec( x+4, y+4, 0);
+			h = vec( x+4, y-4, 0);
 
 			main_block->draw_quadrilateral_hexahedron( a, b, c, d, e, f, g, h, material2);
 
 		}
 	}
 
-	cout << "xy plane done" << endl;
-
-	for(int x = 31; x < 512; x+=64)
+	for(int x = 223; x < 288; x+=64)
 	{
-		for(int z = 31; z < 256; z+=64)
+		for(int z = 95; z < 160; z+=64)
 		{
 
-			a = vec( x-5, 256, z+5);
-			b = vec( x-5,   0, z+5);
-			c = vec( x+5, 256, z+5);
-			d = vec( x+5,   0, z+5);
-			e = vec( x-5, 256, z-5);
-			f = vec( x-5,   0, z-5);
-			g = vec( x+5, 256, z-5);
-			h = vec( x+5,   0, z-5);
+			a = vec( x-4, 256, z+4);
+			b = vec( x-4,   0, z+4);
+			c = vec( x+4, 256, z+4);
+			d = vec( x+4,   0, z+4);
+			e = vec( x-4, 256, z-4);
+			f = vec( x-4,   0, z-4);
+			g = vec( x+4, 256, z-4);
+			h = vec( x+4,   0, z-4);
 
 			main_block->draw_quadrilateral_hexahedron( a, b, c, d, e, f, g, h, material2);
 
 		}
 	}
 
-	cout << "xz plane done" << endl;
-
-	for(int y = 31; y < 256; y+=64)
+	for(int y = 95; y < 160; y+=64)
 	{
-		for(int z = 31; z < 256; z+=64)
+		for(int z = 95; z < 160; z+=64)
 		{
 
-			a = vec(   0, y+5, z+5);
-			b = vec(   0, y-5, z+5);
-			c = vec( 512, y+5, z+5);
-			d = vec( 512, y-5, z+5);
-			e = vec(   0, y+5, z-5);
-			f = vec(   0, y-5, z-5);
-			g = vec( 512, y+5, z-5);
-			h = vec( 512, y-5, z-5);
+			a = vec(   0, y+4, z+4);
+			b = vec(   0, y-4, z+4);
+			c = vec( 512, y+4, z+4);
+			d = vec( 512, y-4, z+4);
+			e = vec(   0, y+4, z-4);
+			f = vec(   0, y-4, z-4);
+			g = vec( 512, y+4, z-4);
+			h = vec( 512, y-4, z-4);
 
 			main_block->draw_quadrilateral_hexahedron( a, b, c, d, e, f, g, h, material2);
 
 		}
 	}
-
-	cout << "yz plane done" << endl;
-
-
-
-
-
-
-
-
-	cout << endl << "moving to inner layers" << endl << endl;
 
 
 	//INNER LAYERS
 
-	for(int x = 31; x < 512; x+=64)
+	for(int x = 223; x < 288; x+=64)
 	{
-		for(int y = 31; y < 256; y+=64)
+		for(int y = 95; y < 160; y+=64)
 		{
 
 			a = vec( x-3, y+3, 256);
@@ -240,11 +139,9 @@ int main()
 		}
 	}
 
-	cout << "xy plane done" << endl;
-
-	for(int x = 31; x < 512; x+=64)
+	for(int x = 223; x < 288; x+=64)
 	{
-		for(int z = 31; z < 256; z+=64)
+		for(int z = 95; z < 160; z+=64)
 		{
 
 			a = vec( x-3, 256, z+3);
@@ -261,11 +158,9 @@ int main()
 		}
 	}
 
-	cout << "xz plane done" << endl;
-
-	for(int y = 31; y < 256; y+=64)
+	for(int y = 95; y < 160; y+=64)
 	{
-		for(int z = 31; z < 256; z+=64)
+		for(int z = 95; z < 160; z+=64)
 		{
 
 			a = vec(   0, y+3, z+3);
@@ -282,53 +177,32 @@ int main()
 		}
 	}
 
-	cout << "yz plane done" << endl << endl;;
-
-	main_block->mask_by_state(27);
-
-	main_block->mask_invert_mask();
-
-	main_block->draw_perlin_noise( 0.1, 0.3, material3);
-
-	main_block->draw_perlin_noise( 0.1, 0.25, rust);
-
-	main_block->draw_perlin_noise( 0.3, 0.4, rust2);
-
-	main_block->mask_unmask_all();
-
 	main_block->mask_all_nonzero();
 
-	main_block->draw_sphere(vec(255.5, 127.5, 127.5), 1000, atmosphere);
-
-	a = vec(   0,   175, 256);
-	b = vec(   0,     0, 256);
-	c = vec( 512,   175, 256);
-	d = vec( 512,     0, 256);
-	e = vec(   0,   175, 0);
-	f = vec(   0,     0, 0);
-	g = vec( 512,   175, 0);
-	h = vec( 512,     0, 0);
-
-	main_block->draw_quadrilateral_hexahedron( a, b, c, d, e, f, g, h, water);
+	main_block->draw_sphere(vec(255.5, 127.5, 127.5), 1000, main_block->get_vox(58, 1.0, false));
 
 	main_block->mask_unmask_all();
 
-	main_block->mask_by_state(40);
 
-	main_block->mask_invert_mask();
 
-	main_block->draw_perlin_noise( 0.3, 0.4, atmosphere);
+
+
 
 
 
 
 
 //FRAMING
-  bool draw_axes = false;
-  if( draw_axes )
+  if( false )
   {
     draw_axes_and_corners();
   }
+
+//COMPASS ROSE
+	if(true)
+	{
+		draw_compass_rose();
+	}
 
 
 
@@ -396,4 +270,38 @@ void draw_axes_and_corners()
   main_block->draw_sphere(vec(init_x-1,0,init_z-1), 3, main_block->get_vox(62,255,false));
   main_block->draw_sphere(vec(init_x-1,init_y-1,init_z-1), 3, main_block->get_vox(62,255,false));
 
+}
+
+void draw_compass_rose()
+{
+	//the compass rose
+
+	main_block->draw_ellipsoid(vec(255.5, 127.5, 127.5), vec(63, 13, 13), main_block->get_vox(62, 20.0, false));
+	main_block->draw_ellipsoid(vec(255.5, 127.5, 127.5), vec(13, 63, 13), main_block->get_vox(62, 20.0, false));
+	main_block->draw_ellipsoid(vec(255.5, 127.5, 127.5), vec(13, 13, 63), main_block->get_vox(62, 20.0, false));
+
+	main_block->draw_ellipsoid(vec(255.5, 127.5, 127.5), vec(60, 10, 10), main_block->get_vox( 1, 255.0, false));
+	main_block->draw_ellipsoid(vec(255.5, 127.5, 127.5), vec(10, 60, 10), main_block->get_vox(23, 255.0, false));
+	main_block->draw_ellipsoid(vec(255.5, 127.5, 127.5), vec(10, 10, 60), main_block->get_vox(33, 255.0, false));
+
+	// note:
+	// 	a : -x, +y, +z
+	// 	b : -x, -y, +z
+	// 	c : +x, +y, +z
+	// 	d : +x, -y, +z
+	// 	e : -x, +y, -z
+	// 	f : -x, -y, -z
+	// 	g : +x, +y, -z
+	// 	h : +x, -y, -z
+
+	vec a = vec( 255.5-20, 127.5+20, 127.5+20);
+	vec b = vec( 255.5-20, 127.5-20, 127.5+20);
+	vec c = vec( 255.5+20, 127.5+20, 127.5+20);
+	vec d = vec( 255.5+20, 127.5-20, 127.5+20);
+	vec e = vec( 255.5-20, 127.5+20, 127.5-20);
+	vec f = vec( 255.5-20, 127.5-20, 127.5-20);
+	vec g = vec( 255.5+20, 127.5+20, 127.5-20);
+	vec h = vec( 255.5+20, 127.5-20, 127.5-20);
+
+	main_block->draw_quadrilateral_hexahedron( a, b, c, d, e, f, g, h, main_block->get_vox(58, 1.0, false));
 }
